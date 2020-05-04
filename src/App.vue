dce3bd49567b0f6fcb0c0917dfe88d7394f9d053
Otto <otto@masterbranch.io>
2020-05-04 09:35:42 +0100

    justify-content-center

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="justify-content-center" >
     <section v-if="errored">
       <p>We're sorry, we're not able to retrieve this information at the moment, please try back later</p>
     </section>
